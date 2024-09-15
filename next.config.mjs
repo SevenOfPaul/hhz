b38f54e67540b6b923feeffb763c220b420f76b4
paul <13136918273@gmail.com>
2024-09-15 14:36:50 +0800

    完成基础table

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 
-if (process.env.NODE_ENV === 'development') {
-
-await setupDevPlatform()
-
-}
 
 const nextConfig = {}
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,51 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import {Skeleton} from "antd"
-import axios from "../request/index";
-const json=[
-  {name:"游戏名",android:"安卓",pc:"pc",info:"备注"}
-]
+import {Button, Table} from "antd"
+import process from "process"
+import fs from "node:fs"
+import path from "path";
+const curPth=process.cwd();
+const games=JSON.parse(fs.readFileSync(path.join(curPth,"./src/app/games.json5")));
+const columns = [
+  {
+    title: '游戏名',
+    dataIndex: 'name',
+    width: 200,
+  },
+  {
+    title: '安卓链接',
+    dataIndex: 'andriod',
+    width: 300,
+  },
+  {
+    title: 'pc下载链接',
+    dataIndex: 'pc',
+    width: 200,
+  },{
+    title: '备注',
+    dataIndex: 'info',
+    width: 200,
+  }
+];
 export default async function Home() {
-  // const games=await axios.get("/");
+  const info = (title,content) => {
+    Modal.info({
+      title,
+      content: (
+        <div>
+          <p>{content}</p>
+        </div>
+      ),
+      onOk() {},
+    });
+  };
   return (
     <div className={styles.page}>
-  <Skeleton   paragraph={{ rows: 7}} />
+      <h1>游戏合集</h1>
+      <div className={styles.header}>
+      {/* <Button type="primary" onClick={()=>info("关注博主")}>关注博主</Button>
+        <Button type="primary" onClick={()=>info("加入我们","q群：221666602")}>加入我们</Button> */}
+      </div>
+      <Table columns={columns} dataSource={games} pagination={{ pageSize: 50 }} scroll={{ y: 240 }} />
     </div>
   );
 }
