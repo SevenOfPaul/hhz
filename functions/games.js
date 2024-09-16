d235ca2af959a46d9c5a431a4f9c8fb89dbac9b3
paul <13136918273@gmail.com>
2024-09-16 17:15:03 +0800

    测试json

--- functions/games.js
+++ functions/games.js
@@ -1,4 +1,8 @@
+import process from "process"
+import fs from "node:fs"
+import path from "path";
 export function onRequest(context) {
-  context.env.hhz.set("games",[""])
-    return new Response(JSON.stringify(context.env.hhz.get("ki"))+JSON.stringify(context.env.hhz.get))
+  const curPth=process.cwd();
+  const games=fs.readFileSync(path.join(curPth,"./src/pages/games.json5"));
+  return new Response(JSON.stringify(games));
   }
