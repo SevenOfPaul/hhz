d5bf56a9dbb00ee8e8d3ddee11a468247ff911eb
paul <13136918273@gmail.com>
2024-09-16 17:27:33 +0800

    测试kv

--- functions/games.js
+++ functions/games.js
@@ -1,8 +1,6 @@
-import process from "node:process"
-import fs from "node:fs"
-import path from "node:path";
-export function onRequest(context) {
-  const curPth=process.cwd();
-  const games=fs.readFileSync(path.join(curPth,"./src/pages/games.json5"));
-  return new Response(JSON.stringify(games));
+
+export function onRequest({request,env}) {
+  env.hhz.put("l0",JSON.stringify("你好，1"))
+  env.hhz.put("k2",JSON.stringify("你好，1"))
+  return new Response(JSON.stringify(context.env.hhz.get("ki")));
   }
