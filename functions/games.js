4c5a0997efd8bbaf85c435b2e2ba8c151b372789
paul <13136918273@gmail.com>
2024-09-16 16:57:59 +0800

    测试函数3

--- functions/games.js
+++ functions/games.js
@@ -1,4 +1,4 @@
 export function onRequest(context) {
      context.env.games=[1,2,3,4,5,6]
-    return new Response(JSON.stringify(context.games)+7)
+    return new Response(JSON.stringify(context.env)+7)
   }
