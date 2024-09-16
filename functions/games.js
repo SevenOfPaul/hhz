f48f352d9d5d0b6bf9b41c65921965b7d0cc2b5e
paul <13136918273@gmail.com>
2024-09-16 17:13:44 +0800

    接着测试3

--- functions/games.js
+++ functions/games.js
@@ -1,4 +1,4 @@
 export function onRequest(context) {
-
+  context.env.hhz.set("games",[""])
     return new Response(JSON.stringify(context.env.hhz.get("ki"))+JSON.stringify(context.env.hhz.get))
   }
