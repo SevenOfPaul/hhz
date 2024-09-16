65ddfc2cf34fc16c27b76c771814ecc651527263
paul <13136918273@gmail.com>
2024-09-16 16:57:10 +0800

    修改function

--- functions/games.js
+++ functions/games.js
@@ -1,4 +1,4 @@
 export function onRequest(context) {
-  context.games=[1,2,3,4,5,6]
-    return new Response(JSON.stringify(context.games))
+     context.env.games=[1,2,3,4,5,6]
+    return new Response(JSON.stringify(context.games)+7)
   }
