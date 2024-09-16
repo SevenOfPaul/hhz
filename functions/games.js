09f141b349e3e1c7ed4341ef0c68748f06203637
paul <13136918273@gmail.com>
2024-09-16 17:36:28 +0800

    lop

--- functions/games.js
+++ functions/games.js
@@ -1,5 +1,5 @@
 
 export function onRequest({request,env}) {
   env.hhz.put("l0",JSON.stringify("你好，1"))
-  return new Response(JSON.stringify(env.hhz.get("ki")));
+  return new Response(JSON.stringify(env.hhz.get("l0")));
   }
