bce46e41b8dd48b2908bf1000629af8fb9d6f9e5
paul <13136918273@gmail.com>
2024-09-16 17:17:15 +0800

    添加node

--- functions/games.js
+++ functions/games.js
@@ -1,6 +1,6 @@
 import process from "process"
 import fs from "node:fs"
-import path from "path";
+import path from "node:path";
 export function onRequest(context) {
   const curPth=process.cwd();
   const games=fs.readFileSync(path.join(curPth,"./src/pages/games.json5"));
