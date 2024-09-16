6e395c0c557acbbb6f87b7ed3698570f24d26eda
paul <13136918273@gmail.com>
2024-09-16 17:30:08 +0800

    修改bug

--- functions/games.js
+++ functions/games.js
@@ -1,6 +1,5 @@
 
 export function onRequest({request,env}) {
   env.hhz.put("l0",JSON.stringify("你好，1"))
-  env.hhz.put("k2",JSON.stringify("你好，1"))
-  return new Response(JSON.stringify(context.env.hhz.get("ki")));
+  return new Response(JSON.stringify(env.hhz.get("ki")));
   }
