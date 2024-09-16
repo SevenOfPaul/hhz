6cd695ee2eb8e8c18bd77ee9ed4082b67542572d
paul <13136918273@gmail.com>
2024-09-16 17:11:03 +0800

    测试kv

--- functions/games.js
+++ functions/games.js
@@ -1,4 +1,4 @@
 export function onRequest(context) {
 
-    return new Response(JSON.stringify(context.env.hhz.get("ki")))
+    return new Response(JSON.stringify(context.env.hhz.get("ki")+JSON.stringify(context.env.hhz.get))
   }
