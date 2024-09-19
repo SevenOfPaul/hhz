fc068eb430f9c80e5790dd14f1e6a20ed9bf7660
paul <13136918273@gmail.com>
2024-09-19 20:21:37 +0800

    大更新

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,43 +2,41 @@ import styles from "./page.module.css";
 import { Table } from "antd"
 import Header from "@/components/Header";
 import process from "process"
-import fs from "node:fs"
-import path from "path";
-// import axios from "../request/index";
-const columns = [
-  {
-    title: '游戏名',
-    dataIndex: 'name',
-    width: 200,
-  },
-  {
-    title: '安卓链接',
-    dataIndex: 'andriod',
-    width: 300,
-  },
-  {
-    title: 'pc下载链接',
-    dataIndex: 'pc',
-    width: 200,
-  },{
-    title: '备注',
-    dataIndex: 'info',
-    width: 200,
-  }
-];
-export const getStaticProps = (async (context) => {
-    //  const res=await axios("/games");
-    //  console.log(res)
-  const curPth=process.cwd();
-  const games=fs.readFileSync(path.join(curPth,"./src/pages/games.json5"));
-  return { props:{games:games.toString()} };
-})
+import axios from "../request/index";
+import { useEffect, useState } from "react";
 export default function Home(props) {
-  const games=JSON.parse(props.games);
+  const columns = [
+    {
+      title: '游戏名',
+      dataIndex: 'name',
+      width: 200,
+    },
+    {
+      title: '安卓链接',
+      dataIndex: 'andriod',
+      width: 300,
+    },
+    {
+      title: 'pc下载链接',
+      dataIndex: 'pc',
+      width: 200,
+    },{
+      title: '备注',
+      dataIndex: 'info',
+      width: 200,
+    }
+  ];
+  let [data,setData]=useState({loading:true,games:[]});
+  
+   useEffect(async ()=>{
+    const games=await axios.get("/");
+    setData({loading:false,games});
+   },[])
   return (
+    
     <div className={styles.page}>
        <Header title={"游戏合集"}/>
-      <Table columns={columns} dataSource={games} pagination={{ pageSize: 30 }} scroll={{ y: 400 }} />
+     {data.loading?"":<Table columns={columns}  dataSource={data.games} pagination={{ pageSize: 30 }} scroll={{ y: 400 }} />}
     </div>
   );
 }
