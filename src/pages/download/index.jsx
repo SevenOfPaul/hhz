99caa17348adc9b06bfe43963d45e9bff03cc457
paul <13136918273@gmail.com>
2024-09-22 23:58:16 +0800

    6

--- src/pages/download/index.jsx
+++ src/pages/download/index.jsx
@@ -1,42 +1,65 @@
 import React from 'react'
 import Head from "next/head";
-import { useSearchParams } from 'next/navigation'
 import { useEffect, useState,startTransition } from "react";
 import Header from "@/components/Header";
 import styles from "./download.module.css"
+<<<<<<< HEAD
 import { Input,message,List,Form,Button  } from 'antd';
+=======
+import { Modal,Input,message,List  } from 'antd';
+>>>>>>> parent of 75e1cdd (拆分逻辑)
 import ico from "../../public/favicon.ico"
-import parseQuery from './parseQuery';
-import init from "./init"
+import axios from '../../request';
 export default function index() {
-  const searchParams = useSearchParams()
-  const url = searchParams.get('url')
-  let initValue={};
-  let [gameUrl,setGameUrl]=useState([]);
-
+  let codeInput="";
+  const error = () => {
+    message.open({
+      type: 'error',
+      content: '验证码错误，请关注微信公众号《游戏资讯官》获取',
+      style: {
+        zIndex: '101',
+      },
+    });
+  };
 
   useEffect(()=>{
+<<<<<<< HEAD
    init();
   try{
+=======
+>>>>>>> parent of 75e1cdd (拆分逻辑)
     (async()=>{
-      let parseResult=await parseQuery(url);
-       initValue.url=parseResult.url;
-       initValue.pwd=parseResult.pwd;
-       initValue.surl=parseResult.surl;
-       console.log(initValue)
-    })()
-  }catch(e){
-    
+   const code=(await axios.get("/code")).code;
+   function ok(codeInput,code){
+       if(code==codeInput){
+        return Promise.resolve()
+       }else{
+        startTransition(()=>{
+          error();
+        })
+        return Promise.reject()
+       }
+   }
+   const config={
+    title: '验证码已过期，请输入验证码',
+    mask:true,
+    keyboard:false,
+    zIndex:100,
+    content: (
+        <Input onChange={(e)=>{
+          codeInput=e.target.value;
+          localStorage.setItem("code",codeInput);
+        }}/>
+    ),
+    onOk:()=>ok(codeInput,code),
+    onCancel:()=>{}
   }
-},[url])
-console.log(initValue)
-const onFinish = (values) => {
-  console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
+  console.log("error")
+   if((localStorage.getItem("code")||"")!==code){
+    Modal.info(config);
+   }
+  })();
+},[])
   return (
     <div className={styles.download}>
                     <Head>
@@ -46,58 +69,19 @@ const onFinishFailed = (errorInfo) => {
       <Header title={"加速下载"}>
       {/* <Button type="primary" onClick={submit}>提交</Button> */}
       </Header>
-      <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={initValue}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="链接"
-      name="url"
-      rules={[{ required: true, message: '请输入链接!' }]}
-    >
-      <Input/>
-    </Form.Item>
-
-    <Form.Item
-      label="提取码"
-      name="pwd"
-      rules={[{ required: true, message: '提取码!' }]}
-    >
-      <Input/>
-    </Form.Item>
-    <Form.Item
-    style={{display:"none"}}
-      label="surl"
-      name="surl"
-      rules={[{ required: true, message: '解析码!' }]}
-    >
-      <Input/>
-    </Form.Item>
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        提交
-      </Button>
-    </Form.Item>
-  </Form>
       <div className={styles.content}>
-      {gameUrl.length? <List
+      <List
     itemLayout="horizontal"
-    dataSource={gameUrl}
+    dataSource={["你哈","你哈你哈"]}
     renderItem={(item, index) => (
       <List.Item>
         <List.Item.Meta onClick={async()=>await navigator.clipboard.writeText(item)}
-          title={<p style={{height:"12px",color:"black"}}>{item.url}</p>}
-          description={item.parseUrl}
+          title={<p style={{height:"12px",color:"black"}}>{item}</p>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
         />
       </List.Item>
     )}
-  />:<></>}
+  />
       </div>
     </div>
   )
