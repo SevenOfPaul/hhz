569c62fdb383254c0e2f04ef301d16cc6847cb30
paul <13136918273@gmail.com>
2024-09-28 22:32:31 +0800

    修改密码

--- src/pages/download/index.jsx
+++ src/pages/download/index.jsx
@@ -16,7 +16,7 @@ export default function index() {
       <Header title={"加速下载"}>
       <p style={{padding:'3px'}}>我这就是个转发页面，欢迎大家直接联系原作者 </p>
       <Link style={{padding:'3px'}} href="../docs">教程地址</Link>
-      <Link style={{padding:'3px'}} href="https://wwqg.lanzoub.com/i7wOX2al8jef">软件下载地址 密码为6u3d</Link>
+      <Link style={{padding:'3px'}} href="https://wwqg.lanzoub.com/i7wOX2al8jef">软件下载地址 密码为63ud</Link>
       </Header>
     
     <iframe className={styles.iframe} src={"https://pan.gt288.top"}></iframe>
