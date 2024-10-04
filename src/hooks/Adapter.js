bff9dbc5eac4b4fe4691522e95488d72d64cf335
paul <13136918273@gmail.com>
2024-10-04 13:04:24 +0800

    修改

--- src/hooks/Adapter.js
+++ src/hooks/Adapter.js
@@ -2,6 +2,7 @@ import { nanoid } from "nanoid";
 
 export function Gamepad(obj){
     obj.id=nanoid();
+    obj.name=obj.name||""
     obj.android=obj.android||""
     obj.pc=obj.pc||""
     obj.info=obj.info||""
