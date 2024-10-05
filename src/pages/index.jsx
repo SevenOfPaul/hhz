b343365dc4019c5708dd397d12c80d2cd007f92c
paul <13136918273@gmail.com>
2024-10-05 13:43:57 +0800

    提交

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -61,7 +61,7 @@ export default function Home() {
   }
   }
    useEffect(async ()=>{
-    const games=(await axios.get("/game",{params:{page:page}})).games;
+    const games=(await axios.get("/game",{params:{page:1}})).games;
     setDefaultGames(games);
     setData({loading:false,games});
     // return ()=>{}
