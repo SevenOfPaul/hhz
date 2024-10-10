6b8ccb5a342ecf44bc07230de2537c1f03c716cc
paul <13136918273@gmail.com>
2024-10-10 18:52:35 +0800

    提交验证码

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import styles from "./page.module.css";
-import { Table,Skeleton,Input } from "antd"
+import { Table,Skeleton,Input,Modal,message } from "antd"
 import Link from 'next/link'
 import Header from "@/components/Header";
 import axios from "../request/index";
 import { useEffect, useState } from "react";
 import Meta from "@/components/Meta";
+import img from "../public/gzh.jpg"
 import { code1HasCode2 } from '@/hooks/searchHook'
 export default function Home() {
   const columns = [
@@ -60,7 +61,30 @@ export default function Home() {
 //     setPage(newPage-1);
 //   }
 //   }
+const [isModalOpen, setIsModalOpen] = useState(false);
+const [messageApi, contextHolder] = message.useMessage();
+const showModal = () => {
+  setIsModalOpen(true);
+};
+const [code,setCode]=useState("");
+function changeCode(e){
+  setCode(e.target.value)
+}
+const handleOk = () => {
+  if(code=="9999"){
+    setIsModalOpen(false);
+    localStorage.setItem("code",9999);
+  }else{
+    messageApi.open({
+      type: 'warning',
+      content: '验证码输入错误，请关注博主！',
+    });
+  }
+};
    useEffect(async ()=>{
+    if(localStorage.getItem("code")!='9999'){
+      showModal();
+    }
     const games=(await axios.get("/game",{params:{page:page}})).games;
     setDefaultGames(games);
     setData({loading:false,games});
@@ -68,6 +92,13 @@ export default function Home() {
    },[])
   return (
     <div className={styles.page}>
+         {contextHolder}
+      <Modal title="很抱歉" open={isModalOpen} okText={"提交"} onOk={handleOk} closable={false}cancelButtonProps={{ style:{display:"none"}}}>
+        <p>很抱歉打扰到您，但是因为公众号被举报，我们只能以这样的一种方式强制您关注公众号。您放心这是一种无奈之举，不会长时间如此。</p>
+        <img src={img.src} />
+        <p>请输入验证码，验证码在关注公众号后发送验证码或点击菜单皆可</p>
+        <Input placeholder="请输入验证码" onChange={(e)=>changeCode(e)}/>
+      </Modal>
    <Meta title={"游戏合集，你喜欢的游戏都在这里"}>
      <meta property="og:title" content={defaultGames.map(g=>g.name).join(" ")} key={defaultGames.map(g=>g.name).join(" ")} /></Meta>
        <Header title={"游戏合集"}>
