6ec7122b5a67c8a850b61f453e3f99c867a54ae5
paul <13136918273@gmail.com>
2024-10-14 21:27:54 +0800

    提交图片打包模式

--- src/pages/gameDocs/index.jsx
+++ src/pages/gameDocs/index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import docs01 from "../../public/docs01.jpg"
+import docs02 from "../../public/docs02.jpg"
+import docs03 from "../../public/docs03.jpg"
 import Header from "@/components/Header";
 import styles from "./gameDocs.module.css";
 import Meta from "../../components/Meta";
 import { List, Image } from "antd";
-import docs from "./docs";
+import docs from "./docs.json" assert {type: "json"}
+const imgs={
+    docs01:docs01,
+    docs02: docs02,
+    docs03:docs03
+}
 export default function index() {
   return (
     <div className={styles.docs}>
@@ -21,7 +29,7 @@ export default function index() {
             <List.Item
               key={item.question}
               extra={
-                <Image width={272} alt="logo" src={item.img.src} />
+                <Image width={272} alt="logo" src={imgs[item.img].src} />
               }
             >
               <List.Item.Meta
