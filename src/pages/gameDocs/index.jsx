1940a68d0c2fdb7838a845cf300bb4c7c74aecfd
paul <g13136918273@gmail.com>
2024-10-29 11:56:15 +0800

    增加唾骂

--- src/pages/gameDocs/index.jsx
+++ src/pages/gameDocs/index.jsx
@@ -3,6 +3,7 @@ import docs01 from "../../public/docs01.jpg"
 import docs02 from "../../public/docs02.jpg"
 import docs03 from "../../public/docs03.jpg"  
 import docs04 from "../../public/docs04.jpg"
+import docs05 from "../../public/docs05.jpg"
 import Header from "@/components/Header";
 import styles from "./gameDocs.module.css";
 import Meta from "../../components/Meta";
@@ -12,7 +13,8 @@ const imgs={
     docs01:docs01,
     docs02: docs02,
     docs03:docs03,
-    docs04:docs04
+    docs04:docs04,
+    docs04:docs05
 }
 export default function index() {
   return (
